6367938b07ff62063bb9e2353dfa59040e95509f
Tch1b0 <Jpour3006@gmail.com>
2021-12-31 16:06:05 +0100

    Add comments and enhance code

--- utility/levels.py
+++ utility/levels.py
@@ -1,17 +1,30 @@
-from utility.utility import Coordinates, GameField, Player
+from utility.utility import Coordinates, GameField
 
 class LevelInterpreter():
+    """
+    An interpreter used for generating GameFields from a
+    configuration file
+    """
+
     def __init__(self, file_path) -> None:
         with open(file_path, "r") as f:
             self.level_code: list[list[str]] = [x.split(" ", 1) for x in f.readlines() if x not in [" ", "\n", ""]]
 
     def build_game_field(self) -> GameField:
+        """
+        Build a new GameField from the loaded file
+        """
+
         game_field: GameField
+        try:
+            raw_size = list(filter(lambda x: x[0] == "siz", self.level_code))[0][1]
+            x, y = [int(x) for x in raw_size.split(",")]
+            game_field = GameField((x, y))
+        except:
+            raise Exception("LevelInterpreter: missing 'siz' command in lvl file")
+
         for command in self.level_code:
             match command[0]:
-                case "siz":
-                    x, y = [int(x) for x in command[1].split(",")]
-                    game_field = GameField((x, y))
                 case "plr":
                     coords = Coordinates(*[int(x) for x in command[1].split(",")])
                     game_field.spawn_player(coords)
