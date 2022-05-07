d318aa078fac6729aadf796747f29272c73ab732
Tch1b0 <Jpour3006@gmail.com>
2022-05-07 09:13:04 +0200

    display time

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+from time import time
 import pygame
 from utility.levels import LevelInterpreter
 from utility.utility import *
@@ -6,7 +7,7 @@ import sys
 
 # define constants
 FIELD_COLORS = {
-    FieldStates.EMPTY: (0,0,0),
+    FieldStates.EMPTY: (0, 0, 0),
     FieldStates.WALL: (150, 150, 150),
     FieldStates.TARGET: (50, 50, 200),
     FieldStates.VISITED: (50, 0, 0)
@@ -26,6 +27,7 @@ window = pygame.display.set_mode([
     len(field.field)*BOX_SIZE      # calculate and set height
 ])
 
+
 def render():
     """
     Render the pygame view
@@ -39,16 +41,28 @@ def render():
         for x, item in enumerate(row):
             if item not in FieldStates.ALL:
                 # item has to be Player
-                pygame.draw.rect(window, (200, 30, 30), (x*BOX_SIZE, y*BOX_SIZE, BOX_SIZE, BOX_SIZE))
+                pygame.draw.rect(window, (200, 30, 30),
+                                 (x*BOX_SIZE, y*BOX_SIZE, BOX_SIZE, BOX_SIZE))
             else:
-                pygame.draw.rect(window, FIELD_COLORS[item], (x*BOX_SIZE, y*BOX_SIZE, BOX_SIZE, BOX_SIZE))
+                pygame.draw.rect(
+                    window, FIELD_COLORS[item], (x*BOX_SIZE, y*BOX_SIZE, BOX_SIZE, BOX_SIZE))
 
     pygame.display.update()
 
+
 if __name__ == "__main__":
+    # get the start time
+    start = time()
+
     # solve maze
     find_path(field, render, 44)
-    
+
+    # get the end time
+    end = time()
+
+    # print the time needed to solve the maze
+    print(f"Solved in {end-start:.3f} seconds")
+
     # keep displaying the (un)solved maze
     while True:
         render()
